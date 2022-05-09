0f7072ab4bf2a30e0902c4bc24d6fd87780aefe3
Otávio Azevedo <otavioad1997@gmail.com>
2022-05-10 00:36:55 +0200

    Requisito 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 const Missions = () => (
   <div data-testid="missions">
     <Title headline="Missões" />
+    <section>
+      {missions.map((planet) => (
+        <MissionCard
+          key={ planet.name }
+          name={ planet.name }
+          year={ planet.year }
+          country={ planet.country }
+          destination={ planet.destination }
+        />
+      ))}
+    </section>
   </div>
 );
 
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -5,19 +5,21 @@ import planets from '../data/planets';
 import Missions from './Missions';
 
 const SolarSystem = () => (
-  <div div data-testid="solar-system">
-    <Title headline="Planetas" />
-    <section>
-      {planets.map((planet) => (
-        <PlanetCard
-          key={ planet.name }
-          planetName={ planet.name }
-          planetImage={ planet.image }
-        />
-      ))}
-    </section>
+  <>
+    <div div data-testid="solar-system">
+      <Title headline="Planetas" />
+      <section>
+        {planets.map((planet) => (
+          <PlanetCard
+            key={planet.name}
+            planetName={planet.name}
+            planetImage={planet.image}
+          />
+        ))}
+      </section>
+    </div>
     <Missions />
-  </div>
+  </>
 );
 
 export default SolarSystem;
