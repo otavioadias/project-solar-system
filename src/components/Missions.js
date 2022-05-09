207d7f9c95cb0a773c22d40a4014d2a362a31db4
Otávio Azevedo <otavioad1997@gmail.com>
2022-05-10 00:13:23 +0200

    Requisito 8

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Title from './Title';
 
-const Missions = () => (<div data-testid="missions">.</div>);
+const Missions = () => (
+  <div data-testid="missions">
+    <Title headline="Missões" />
+  </div>
+);
 
 export default Missions;
