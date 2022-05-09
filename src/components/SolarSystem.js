7c5fa121a123678abcc11aa6022feb7b113e4205
Otávio Azevedo <otavioad1997@gmail.com>
2022-05-10 01:02:54 +0200

    Correções

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -5,21 +5,18 @@ import planets from '../data/planets';
 import Missions from './Missions';
 
 const SolarSystem = () => (
-  <>
-    <div div data-testid="solar-system">
-      <Title headline="Planetas" />
-      <section>
-        {planets.map((planet) => (
-          <PlanetCard
-            key={ planet.name }
-            planetName={ planet.name }
-            planetImage={ planet.image }
-          />
-        ))}
-      </section>
-    </div>
-    <Missions />
-  </>
+  <div div data-testid="solar-system">
+    <Title headline="Planetas" />
+    <section>
+      {planets.map((planet) => (
+        <PlanetCard
+          key={ planet.name }
+          planetName={ planet.name }
+          planetImage={ planet.image }
+        />
+      ))}
+    </section>
+  </div>
 );
 
 export default SolarSystem;
